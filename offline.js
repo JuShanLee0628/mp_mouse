b9f8ba713c9272e3bdf0df8db98b48ca612cff54
JuShanLee0628 <119273396+JuShanLee0628@users.noreply.github.com>
2023-04-06 14:20:16 +0800

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1680761695,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite4-sheet1.png",
+		"images/sprite5-sheet0.png",
+		"media/3e5x9-fwyof.m4a",
+		"media/3e5x9-fwyof.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
